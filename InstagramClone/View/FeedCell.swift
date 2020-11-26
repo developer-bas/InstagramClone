425dd3438237620e14bc82ce9e27e8dc16124ba2
Sebastian <cruzguzmaneric@gmail.com>
2020-11-25 23:36:23 -0600

    Finishing Up feed cell UI

--- InstagramClone/View/FeedCell.swift
+++ InstagramClone/View/FeedCell.swift
@@ -29,6 +29,59 @@ class FeedCell : UICollectionViewCell{
         return  button
     }()
     
+    private let postImageView : UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleToFill
+        image.clipsToBounds = true
+        image.isUserInteractionEnabled = true
+        image.image = #imageLiteral(resourceName: "venom-7")
+        return image
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+  
+    private lazy var shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "some text"
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private let postTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 days ago"
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
 //    MARK: - Lifecycle
 
     override init(frame: CGRect){
@@ -43,6 +96,10 @@ class FeedCell : UICollectionViewCell{
         
         addSubview(usernameButton)
         usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+        
+        
+        addSubview(postImageView)
+        postImageView.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8)
     }
     
     required init?(coder: NSCoder) {
