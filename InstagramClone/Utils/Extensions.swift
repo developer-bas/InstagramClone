b31fae272223b26c42c985df40ed029e080010a6
Sebastian <cruzguzmaneric@gmail.com>
2020-11-26 16:14:33 -0600

    Creating Custom subclasses for reusability

--- InstagramClone/Utils/Extensions.swift
+++ InstagramClone/Utils/Extensions.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+extension UIButton {
+    func attributedTittle(fistPart: String, secondPart: String){
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor:UIColor(white: 1, alpha: 0.87),.font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "\(fistPart) ", attributes: atts)
+        
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor:UIColor(white: 1, alpha: 0.87),.font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: "\(secondPart) ", attributes: boldAtts))
+        
+        setAttributedTitle(attributedTitle, for: .normal)
+        
+    }
+}
 
 extension UIView{
     
