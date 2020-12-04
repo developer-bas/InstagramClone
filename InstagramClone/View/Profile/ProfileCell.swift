ee6763be3ebb2c3c7eb2bbfc03a5db1d141f7184
Sebastian <cruzguzmaneric@gmail.com>
2020-12-03 20:35:15 -0600

    Finishing up profile UI

--- InstagramClone/View/Profile/ProfileCell.swift
+++ InstagramClone/View/Profile/ProfileCell.swift
@@ -11,11 +11,23 @@ class ProfileCell: UICollectionViewCell {
     
 //    MARK: - Properties
     
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    
 //    MARK: - Lyfecycle
     override init(frame: CGRect){
         super.init(frame: frame)
     
         backgroundColor = .gray
+        
+        addSubview(postImageView)
+        postImageView.fillSuperView()
     }
     
     required init?(coder: NSCoder) {
