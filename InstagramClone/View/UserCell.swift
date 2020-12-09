c4243a1748a966d43070938e721c41e3d2b92e4f
Sebastian <cruzguzmaneric@gmail.com>
2020-12-08 23:56:08 -0600

    Searching for users

--- InstagramClone/View/UserCell.swift
+++ InstagramClone/View/UserCell.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class UserCell :UITableViewCell{
 //    MARK: - Properties
-    var user: User?{
+    var viewModel : UserCellViewModel?{
         didSet{
-            fullnameLabel.text = user?.fullname
-            usernameLabel.text = user?.username
+            configure()
         }
     }
     
@@ -64,5 +63,12 @@ class UserCell :UITableViewCell{
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+//    MARK: - Helpers
+    func configure(){
+        guard let viewModel = viewModel else {return}
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        usernameLabel.text = viewModel.username
+        fullnameLabel.text = viewModel.fullname
     }
 }
