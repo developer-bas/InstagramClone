8f6d093e8d30f3986b9357978f8e292eff751259
Sebastian <cruzguzmaneric@gmail.com>
2020-12-28 21:53:47 -0600

    Liking a post

--- InstagramClone/Controller/FeedController.swift
+++ InstagramClone/Controller/FeedController.swift
@@ -120,4 +120,16 @@ extension FeedController: FeedCellDelegate{
         let controller = CommentController(post: post)
         navigationController?.pushViewController(controller, animated: true)
     }
+    func cell(_ cell: FeedCell, didLike post: Post) {
+        cell.viewModel?.post.didLike.toggle()
+        
+        if post.didLike {
+            print(" ansbdahs")
+        }else{
+            print(" dhgfsygf ")
+        }
+    }
 }
+
+
+
